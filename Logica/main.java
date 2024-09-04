1548ea3d387d89808844648151dabbc34f06e4ba
Daniel <danieldicelis@gmail.com>
2024-09-04 08:15:50 -0500

    primer commit

---
+++ Logica/main.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Logica;
+import ventanas.Interfaz;
+/**
+ *
+ * @author Daniel Barrios
+ */
+public class main {
+    public static void main(String[] args){
+     Interfaz obj = new Interfaz();
+    obj.setVisible(true);
+    obj.setLocationRelativeTo(null);
+    
+    }
+}
